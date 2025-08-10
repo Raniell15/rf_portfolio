8295d2975ad0e1e198b1a0974233bf210843568a
Raniell Fontiveros <raniellschl@gmail.com>
2025-08-10 14:56:25 +0800

    Test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,32 @@
+import { Check, CheckCheck } from "lucide-react";
 import React from "react";
 
 export const Hero = () => {
   return (
     <section
       id="hero"
-      className="relative min-h-screen flex flex-col items-center justify-center px-4"
+      className="relative min-h-screen min-w-screen flex items-center justify-center px-4"
     >
-      <div className="container flex max-w-4xl mx-auto text-center z-10">
-        <div className="rounded-full overflow-hidden h-100 w-100">
+      <div className="flex space-x-10 max-w-8xl justify-center items-center mx-auto text-center z-10">
+        <div className="rounded-full overflow-hidden h-90 w-90 border-solid border-2 border-primary">
           <img src="../../tmp.jpg" className="h-full w-full object-cover" />
         </div>
-        <div className="chat chat-start">
-          <div className="chat-bubble">test test test</div>
+        <div className="chat chat-start h-ful flex flex-col">
+          <div className="chat-header">
+            <time className="text-sm opacity-50 text-2p mb-2">12:15</time>
+          </div>
+          <div className="relative">
+            <div className="chat-bubble rounded-2xl text-6xl text-2p text-left p-10  pr-36 xl:text-nowrap">Hey! I'm <span >Raniell!</span>
+              <div className="text-xl mt-5">
+                I am a Computer Engineer with a passion for   <br/>
+                software development! I mainly specialize in <br/>
+                Web Development!
+              </div>
+            </div>
+            <CheckCheck className="absolute bottom-0 right-22"/>
+          </div>
         </div>
+        
       </div>
     </section>
   );
