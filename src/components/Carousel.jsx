fc86efa545ed71afe2be2b4e67de4ef1a860c8bf
raniellschl <raniellschl@gmail.com>
2025-08-11 19:27:09 +0800

    Carousel fixed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,39 @@
-import { ArrowLeft, ArrowRight } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 export const Carousel = () => {
   const images = [
-    {src: '../../tmp.jpg', alt: 'Image 1'},
-    {src: '../../tmp2.jpg', alt: 'Image 2'},
-    {src: '../../tmp3.jpg', alt: 'Image 3'},
-  ]
+    { src: "../../tmp.jpg", alt: "Image 1" },
+    { src: "../../tmp2.jpg", alt: "Image 2" },
+    { src: "../../tmp3.jpg", alt: "Image 3" },
+  ];
 
   const [activeImg, setActiveImg] = useState(0);
 
-  const prevImg = () => {
-    setActiveImg(activeImg === 0 ? images.length - 1 : activeImg - 1);
-  }
-
-    const nextImg = () => {
+  const nextImg = () => {
     setActiveImg(activeImg === images.length - 1 ? 0 : activeImg + 1);
-  }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextImg, 5000);
+    return () => clearInterval(interval);
+  });
 
   return (
-    <div className={`flex items-center h-120 max-w-screen overflow-hidden transition-transform duration-500 ease-in-out w-160`} style={{transform: `translateX(-${activeImg * 100 }%)`}}>
+    <div className={`flex items-center h-120  overflow-hidden w-160`}>
+      <div
+        className="flex max-w-screen transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${activeImg * 100}%)` }}
+      >
         {images.map((image) => (
-          <img src={image.src} alt={image.alt} className='h-120 w-200 object-cover'/>
+          <img
+            key={image.src}
+            src={image.src}
+            alt={image.alt}
+            className="h-120 w-200 object-cover"
+          />
         ))}
-        <button className="absolute" onClick={nextImg}>NEXT IMAGE</button>
+      </div>
     </div>
-  )
-}
+  );
+};
