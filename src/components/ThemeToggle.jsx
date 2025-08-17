600fb52c212baf989f1d830830da565ee9cfacad
Raniell Fontiveros <raniellschl@gmail.com>
2025-08-17 18:41:37 +0800

    Default dark mode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -2,32 +2,32 @@ import { Moon, Sun } from "lucide-react";
 import React, { useState, useEffect } from "react";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isLightMode, setIsLightMode] = useState(false);
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
     if (savedTheme === "dark") {
       document.documentElement.classList.add("dark");
-      setIsDarkMode(true);
+      setIsLightMode(false);
     } else {
       document.documentElement.classList.remove("dark");
-      setIsDarkMode(false);
+      setIsLightMode(true);
     }
   }, []);
 
   const toggleTheme = () => {
-    if (isDarkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-      setIsDarkMode(false);
-    } else {
-      document.documentElement.classList.add("dark");
+    if (isLightMode) {
+      document.documentElement.classList.remove("light");
       localStorage.setItem("theme", "dark");
-      setIsDarkMode(true);
+      setIsLightMode(false);
+    } else {
+      document.documentElement.classList.add("light");
+      localStorage.setItem("theme", "light");
+      setIsLightMode(true);
     }
   };
   return (
     <button onClick={toggleTheme}>
-      {isDarkMode ? <Sun color="yellow" /> : <Moon color="blue" />}
+      {isLightMode ? <Moon color="blue" /> : <Sun color="yellow" />}
     </button>
   );
 };
